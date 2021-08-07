c7ae44ad5ab705dd8780cff3fb46911ac1ef5f7c
dumit <dumitru.andrei55@gmail.com>
2021-08-08 01:57:29 +0300

    Setting up Project

--- EcommerceApp.DataAccess/Data/ApplicationDbContext.cs
+++ EcommerceApp.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace EcommerceApp.Data
+namespace EcommerceApp.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- EcommerceApp.Models/ViewModels/ErrorViewModel.cs
+++ EcommerceApp.Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace EcommerceApp.Models
+namespace EcommerceApp.Models.ViewModels
 {
     public class ErrorViewModel
     {
